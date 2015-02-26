58d5c75cb59de918b34de94d300801f7b1d6cf00
Daniel <dds2130@rit.edu>
2015-02-26 12:48:46 -0500

    finished sort method

--- taco/java/src/taco.java
+++ taco/java/src/taco.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Random;
 
 public class taco {
 	public static void main(String[] args) {
@@ -27,7 +28,16 @@ public class taco {
 	}
 	
 	public static void sort(List<Integer> numbers){
-		//TODO
+		int length = numbers.size();
+		Random rand = new Random();
+		
+		//Remove a random element from the list
+		int index = rand.nextInt(length);
+		int number = numbers.remove(index);
+		
+		//Add the element to a random position in the list
+		index = rand.nextInt(length);
+		numbers.add(index, number);		
 	}
 	
 	public static List<Integer> generateNumbers(int amount){
