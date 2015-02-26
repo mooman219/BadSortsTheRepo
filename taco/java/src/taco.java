1e027abc35c29c776434933ae0c8ba7ac4347784
Daniel <dds2130@rit.edu>
2015-02-26 12:37:55 -0500

    frame of code and isSorted finished

--- taco/java/src/taco.java
+++ taco/java/src/taco.java
@@ -1,6 +1,38 @@
+import java.util.List;
 
 public class taco {
 	public static void main(String[] args) {
-		System.out.println("Hello people");
+		int amount = 8;
+		
+		List<Integer> numbers = generateNumbers(amount);
+		
+		loop(numbers);
+	}
+	
+	public static boolean isSorted(List<Integer> numbers){
+
+		for (Integer number : numbers){
+			if (number > numbers.get(number+1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void loop(List<Integer> numbers){
+		int count = 0;
+		while (!isSorted(numbers)){
+			//TODO
+		}
+	}
+	
+	public static void sort(List<Integer> numbers){
+		//TODO
+	}
+	
+	public static List<Integer> generateNumbers(int amount){
+		//TODO
+		return null;
+		
 	}
 }
