945ac1c875448c96900b02533d37ac5ea85ab95d
Wade Mauger <wadeanthony0100@gmail.com>
2015-10-16 21:14:26 -0400

    add initial implementation of sleep sort in python

---
+++ SleepSort/python/sleep-sort.py
@@ -0,0 +1,33 @@
+"""
+Wade Mauger
+October 2015
+
+SleepSort implemented in Python.
+I do what I want. 
+"""
+import threading
+import sys
+from time import sleep
+
+sort_list = []
+
+def validate_list():
+	for num in sys.argv:
+		try:
+			sort_list.append(int(num))
+		except ValueError:
+			pass
+
+
+def sleep_sort(n):
+	sleep(n)
+	print(n, end=", ")
+
+
+def main():
+	validate_list()
+	for num in sort_list:
+		threading.Thread(target=sleep_sort, args=(num, )).start()
+
+
+main()
